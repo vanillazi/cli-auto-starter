4601beacb72712b99e4cf2faba8518981156057d
Vanillazi <vanillazi@wanazi.cn>
2023-09-26 15:10:04 +0800

    从用户目录加载配置文件

--- src/main/java/cn/vanillazi/tool/constant/Constants.java
+++ src/main/java/cn/vanillazi/tool/constant/Constants.java
@@ -6,7 +6,9 @@ import java.nio.file.Path;
 public class Constants {
 
     public static final File DEFAULT_LOGO_FILE =new File("./asset/logo.png");
-    public static final Path DEFAULT_CONF_PATH =Path.of("conf","app.json");
+
     public static final String ICON_PATH ="file:"+DEFAULT_LOGO_FILE.getAbsolutePath();
     public static final String APP_ID= "cli-auto-starter";
+
+    public static final Path DEFAULT_CONF_PATH =Path.of(System.getProperty("user.home"),"."+APP_ID,"app.json");
 }
